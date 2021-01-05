8a75ad24f702b77f7b97fddfa92e02e64e062474
Ed <edwardaacowell@gmail.com>
2021-01-05 11:01:41 +0000

    commiting app.js with firebase imported in and gitignored configfile

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import firebase from "./firebase-config";
 
 export default function App() {
+  const dbh = firebase.firestore();
+
+  dbh.collection("characters").doc("mario")({
+    employment: "robber-king",
+    outfitColor: "red",
+    specialAttack: "fireball",
+  });
+  console.log("test-test");
+
   return (
     <View style={styles.container}>
       <Text>Hiyah!</Text>
