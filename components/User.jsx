1aa94a441b101bb1d4658f834c95cf021e2f52d6
Yuyou Yu <angelayu0906@gmail.com>
2021-01-08 14:07:36 +0000

    made basic layout of user page

--- components/User.jsx
+++ components/User.jsx
@@ -1,8 +1,9 @@
 import React, { useContext } from 'react';
-import { Button, StyleSheet, TextInput, View } from 'react-native';
+import { Button, StyleSheet, TextInput, View, Image, TouchableOpacity, } from 'react-native';
 import { Text } from 'react-native-elements';
 import firebase from '../firebase-config';
 import { UserContext } from './context/user';
+import UserItemList from './UserItemList';
 
 function UserScreen() {
   const { logInUser } = useContext(UserContext);
@@ -18,9 +19,18 @@ function UserScreen() {
 
   return (
     <View style={styles.User}>
-      <Text>{user.displayName}</Text>
-      <Button title="Sign Out" onPress={() => signOutUser()} />
+      <View style={styles.userContainer}>
+        <TouchableOpacity style={styles.signOutButton} onPress={() => signOutUser()} >
+          <Text>Sign Out</Text>
+        </TouchableOpacity>
+        <Image source={{ uri: 'https://reactjs.org/logo-og.png' }} style={{ width: 150, height: 150, borderRadius: 150 }} />
+        <Text style={styles.userName}>{user.displayName}</Text>
+
+      </View>
+
+      <UserItemList />
     </View>
+
   );
 }
 
@@ -28,9 +38,31 @@ const styles = StyleSheet.create({
   User: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'stretch',
+    justifyContent: 'flex-start',
+
+  },
+  userContainer: {
+    marginTop: 40,
+    flex: 0.4,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'lightgray',
+
   },
+  userName: {
+    marginTop: 10,
+    //fontFamily:
+    textTransform: "capitalize"
+  },
+  signOutButton: {
+    borderColor: 'black',
+    borderWidth: 2,
+    marginLeft: 300,
+    borderRadius: 5,
+    padding: 5,
+    backgroundColor: 'lightblue'
+  }
 });
 
 export default UserScreen;
